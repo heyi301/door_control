bec108c8a6979d180a0e5de122b233cc038c11a9
heyi <2970435845@qq.com>
2025-11-06 19:26:05 +0800

    fix clangd

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,6 @@
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=Yes ..
+#! /bin/bash
+
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=Yes
+find build -name compile_commands.json -exec jq -s 'add' {} + > compile_commands.json
+ln -sf $(pwd)/compile_commands.json build/compile_commands.json
+
